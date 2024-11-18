dbc880b92a06f1334ba22744e2903ed9609c9419
sxmcilrath <samuel.xavier.mac@gmail.com>
2024-11-17 22:04:07 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+using DispatchManager.Components;
+using DispatchManager.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Register the DatabaseService before building the application
+builder.Services.AddSingleton(new DatabaseService("Server=localhost;Database=abc_dispatcher_company;User=root;Password=root;"));
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
---
+++ Services/DatabaseService.cs
@@ -0,0 +1,34 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace DispatchManager.Services
+{
+
+    public class DatabaseService
+    {
+        private readonly string _connectionString;
+
+        public DatabaseService(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public DataTable ExecuteQuery(string query)
+        {
+            using var connection = new MySqlConnection(_connectionString);
+            using var command = new MySqlCommand(query, connection);
+            using var adapter = new MySqlDataAdapter(command);
+            var table = new DataTable();
+            adapter.Fill(table);
+            return table;
+        }
+
+        public int ExecuteNonQuery(string query)
+        {
+            using var connection = new MySqlConnection(_connectionString);
+            using var command = new MySqlCommand(query, connection);
+            connection.Open();
+            return command.ExecuteNonQuery();
+        }
+    }
+}
