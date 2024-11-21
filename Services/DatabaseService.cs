30f673cb182481aa13beef31994e4da0294e62c1
sxmcilrath <samuel.xavier.mac@gmail.com>
2024-11-20 22:17:45 -0600

    functional driver login page

--- Services/DatabaseService.cs
+++ Services/DatabaseService.cs
@@ -1,11 +1,9 @@
 ﻿using MySql.Data.MySqlClient;
 using System.Data;
-using MySql.Data.MySqlClient;
-
+using System.Threading.Tasks;
 
 namespace DispatchManager.Services
 {
-
     public class DatabaseService
     {
         private readonly string _connectionString;
@@ -21,6 +19,22 @@ namespace DispatchManager.Services
             using var command = new MySqlCommand(query, connection);
             using var adapter = new MySqlDataAdapter(command);
             var table = new DataTable();
+            adapter.Fill(table);
+            return table;
+        }
+
+        public async Task<DataTable> ExecuteQueryAsync(string query, params MySqlParameter[] parameters)
+        {
+            using var connection = new MySqlConnection(_connectionString);
+            using var command = new MySqlCommand(query, connection);
+            if (parameters != null)
+            {
+                command.Parameters.AddRange(parameters);
+            }
+            using var adapter = new MySqlDataAdapter(command);
+            var table = new DataTable();
+            await connection.OpenAsync();
+            await command.ExecuteNonQueryAsync();
             adapter.Fill(table);
             return table;
         }
